d1b7486e57af9bec3f583cebda0ca7a46a7e0300
mj <kwj4479@gmail.com>
2025-04-08 22:06:33 +0900

    fix: Remove example api

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.v1.example import router as example_router
 from app.api.v1.memo import router as memo_router
 
 router = APIRouter(prefix="/v1")
-router.include_router(example_router)
 router.include_router(memo_router)
--- app/api/v1/example.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/example", tags=["Example"])
-
-
-@router.get("/", response_model=dict)
-def get_hello():
-    return {"message": "Hello World!"}
