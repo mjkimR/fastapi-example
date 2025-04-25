38d34bd7c15f8619772b7f0a3cd9fe49533db2fa
mj <kwj4479@gmail.com>
2025-04-25 21:26:15 +0900

    feat: add app setting variables

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,7 @@
 import functools
 import os
+
+from pydantic import SecretStr, EmailStr, Field
 from pydantic_settings import BaseSettings
 import pathlib
 
@@ -7,7 +9,6 @@ import pathlib
 def get_repo_path():
     """Get the path to the repository."""
     path = str(pathlib.Path(__file__).parent.parent.parent.resolve())
-    print(path)
     return path
 
 
@@ -25,7 +26,13 @@ if os.path.exists(get_env_filename()):
 
 
 class AppSettings(BaseSettings):
-    DATABASE_URL: str = os.getenv("DATABASE_URL", f"sqlite+aiosqlite:///{get_repo_path()}/.test.db")
+    DATABASE_URL: str = Field(default=f"sqlite+aiosqlite:///{get_repo_path()}/.test.db")
+
+    FIRST_USER_EMAIL: EmailStr
+    FIRST_USER_PASSWORD: SecretStr
+
+    SECRET_KEY: SecretStr  # openssl rand -hex 64
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(default=10)
 
 
 @functools.lru_cache
