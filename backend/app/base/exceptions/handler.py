90a569de1cb2db7fbfe71e067db64b6d0a8fd715
minjae <kwj4479@gmail.com>
2026-02-01 00:28:15 +0900

    feat: Database expcetion handler

--- backend/app/base/exceptions/handler.py
+++ backend/app/base/exceptions/handler.py
@@ -2,8 +2,10 @@ from http import HTTPStatus
 
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
+from sqlalchemy.exc import SQLAlchemyError
 
 from app.base.exceptions.base import CustomException
+from app.base.exceptions.db import database_exception_handler
 from app.core.logger import logger
 
 
@@ -15,26 +17,48 @@ def _get_request_id(request: Request) -> str | None:
         return None
 
 
+def _process_custom_exception(request: Request, exc: CustomException):
+    if exc.trace:
+        logger.exception(f"Error: {exc.log_message}")
+    else:
+        logger.error(f"Error: {exc.log_message}")
+
+    # RFC 7807
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "type": "about:blank",
+            "title": exc.title,
+            "status": exc.status_code,
+            "detail": exc.message,
+            "instance": str(request.url.path),
+            "request_id": _get_request_id(request),
+        },
+    )
+
+
+def _process_general_exception(request: Request, exc: Exception):
+    logger.exception(f"Unknown Error: {exc}")
+    status_code = HTTPStatus.INTERNAL_SERVER_ERROR
+    title = status_code.phrase
+    detail = "An unexpected internal server error occurred."
+    return JSONResponse(
+        status_code=status_code,
+        content={
+            "type": "about:blank",
+            "title": title,
+            "status": status_code,
+            "detail": detail,
+            "instance": str(request.url.path),
+            "request_id": _get_request_id(request),
+        },
+    )
+
+
 def set_exception_handler(app: FastAPI):
     @app.exception_handler(CustomException)
     async def custom_exception_handler(request: Request, exc: CustomException):
-        if exc.trace:
-            logger.exception(f"Error: {exc.log_message}")
-        else:
-            logger.error(f"Error: {exc.log_message}")
-
-        # RFC 7807
-        return JSONResponse(
-            status_code=exc.status_code,
-            content={
-                "type": "about:blank",
-                "title": exc.title,
-                "status": exc.status_code,
-                "detail": exc.message,
-                "instance": str(request.url.path),
-                "request_id": _get_request_id(request),
-            },
-        )
+        return _process_custom_exception(request, exc)
 
     @app.exception_handler(HTTPException)
     async def http_exception_handler(request: Request, exc: HTTPException):
@@ -95,22 +119,18 @@ def set_exception_handler(app: FastAPI):
             },
         )
 
+    @app.exception_handler(SQLAlchemyError)
+    async def sqlalchemy_exception_handler(request: Request, exc: SQLAlchemyError):
+        try:
+            async with database_exception_handler():
+                raise exc
+        except CustomException as e:
+            return _process_custom_exception(request, e)
+        except Exception as e:
+            return _process_general_exception(request, e)
+
     @app.exception_handler(Exception)
     async def general_exception_handler(request: Request, exc: Exception):
-        logger.exception(f"Unknown Error: {exc}")
-        status_code = HTTPStatus.INTERNAL_SERVER_ERROR
-        title = status_code.phrase
-        detail = "An unexpected internal server error occurred."
-        return JSONResponse(
-            status_code=status_code,
-            content={
-                "type": "about:blank",
-                "title": title,
-                "status": status_code,
-                "detail": detail,
-                "instance": str(request.url.path),
-                "request_id": _get_request_id(request),
-            },
-        )
+        return _process_general_exception(request, exc)
 
     return app
